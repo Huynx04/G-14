5a9a620f67165b68b10a4a89e919adc6105cf88f
listimdn10 <121751254+listimdn10@users.noreply.github.com>
2024-05-08 23:22:18 +0700

    frontend
    not done img4

---
+++ Frontend/img4/suggestions.js
@@ -0,0 +1,20 @@
+let suggestions = [
+        
+    "Mì trộn trứng",
+    "Mì trộn trứng xúc xích",
+    "Mì trộn sốt bơ tỏi",
+    "Bánh mì" ,
+    "Bánh bao khoai môn",
+    "Bánh giò",
+    "Bánh bao trứng múi xá xiếu",
+    "Đùi gà chiên",
+    "Đùi gà sốt mắm tỏi", 
+    "Đùi gà góc tư xối mỡ",
+    "Kem việt quất",
+    "Kem chocolate",
+    "Hamburger bò",
+    "Hamburger gà",
+    
+
+];
+
