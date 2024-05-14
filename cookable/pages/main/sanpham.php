996a56be331df1f49ab3b531c7859547bd553755
Huynx04 <nxuanhuy992004@gmail.com>
2024-05-14 17:48:48 +0700

    update

--- cookable/pages/main/sanpham.php
+++ cookable/pages/main/sanpham.php
@@ -7,16 +7,19 @@ while ($row_chitiet = mysqli_fetch_array($query_chitiet)){
 ?>
 <div class="wrapper_chitiet">
 <div class="hinhanh_sanpham">
-    <img width="40%" src="../admin/modules/quanlysp/uploads/<?php echo $row_chitiet['hinhanh'] ?>">
+    <img width="100%" src="../admin/modules/quanlysp/uploads/<?php echo $row_chitiet['hinhanh'] ?>">
 </div>
-
+<form method="POST" action="#">
 <div class="chitiet_sanpham">
-    <h3>tên sản phẩm :<?php echo $row_chitiet['tensanpham'] ?></h3>
+    <h3 style="margin:0"></h3>tên sản phẩm :<?php echo $row_chitiet['tensanpham'] ?></h3>
     <p>mã sp :<?php echo $row_chitiet['masp'] ?></p>
     <p>giá sp :<?php echo number_format($row_chitiet['giasp'],0,',','.').'vnđ' ?></p>
     <p>số lượng sp :<?php echo $row_chitiet['soluong'] ?></p>
     <p>danh mục sp :<?php echo $row_chitiet['tendanhmuc'] ?></p>
+    <p><input class="themgiohang" type="submit" value="thêm giỏ hàng"></p>
+    </form>
 </div>
+
 <?php
 }
 ?>
