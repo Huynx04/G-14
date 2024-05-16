166ccedc2a6945be3413e9db95f69b0adcdaaef8
listimdn10 <121751254+listimdn10@users.noreply.github.com>
2024-05-17 00:08:35 +0700

    Update index.php

--- cookable/pages/main/index.php
+++ cookable/pages/main/index.php
@@ -4,17 +4,17 @@ $query_pro = mysqli_query($mysqli, $sql_pro);
 
 
 ?>
-<h3>sản phẩm mới nhẩt</h3>
+<h3 style="margin: 3%;">Sản phẩm mới nhất</h3>
                <ul class="product_list">
                 <?php
                 while ($row = mysqli_fetch_assoc($query_pro)){
                 ?>
-                     <li>
-                        <a href="sanpham.php?quanly=sanpham&id=<?php echo $row['id_sanpham'] ?>">
+                     <li class="product_items">
+                        <a href="sanpham.php?quanly=sanpham&id=<?php echo $row['id_sanpham'] ?>" class="product_item">
                             <img src="../admin/modules/quanlysp/uploads/<?php echo $row['hinhanh'] ?>" alt="">
-                            <p class="title_product">Tên món : <?php echo $row['tensanpham'] ?></p>
-                            <p class="price_product">giá: <?php echo number_format($row['giasp'], 0, ',', '.') . 'vnđ' ?></p>
-                            <p style="text-align:center;color:#d1d1d1 "><?php echo $row['tendanhmuc'] ?></p>
+                            <p class="title_product text_edit1"> <?php echo $row['tensanpham'] ?></p>
+                            <p class="price_product text_edit1"><?php echo number_format($row['giasp'], 0, ',', '.') . 'vnđ' ?></p>
+                            <!-- <p><?php echo $row['tendanhmuc'] ?></p> -->
 
                         </a>
                      </li>
