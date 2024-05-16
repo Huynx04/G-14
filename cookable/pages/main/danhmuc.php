a0c7726895eb43a323bb9e24e74f77d21c95d919
listimdn10 <121751254+listimdn10@users.noreply.github.com>
2024-05-17 00:08:01 +0700

    Update danhmuc.php

--- cookable/pages/main/danhmuc.php
+++ cookable/pages/main/danhmuc.php
@@ -5,20 +5,21 @@ $sql_cate = "SELECT * FROM tbl_danhmuc WHERE tbl_danhmuc.id_danhmuc='$_GET[id]' 
 $query_cate = mysqli_query($mysqli, $sql_cate);
 $row_title = mysqli_fetch_array($query_cate);
 ?>
-
-<h3>danh mục món ăn :<?php echo $row_title['tendanhmuc'] ?> </h3>
+<div class="container-fluid">
+<h3>Danh mục món ăn: <?php echo $row_title['tendanhmuc'] ?> </h3>
 <ul class="product_list">
     <?php
     while ($row_pro = mysqli_fetch_array($query_pro)) {
-    ?>
-        <li>
-            <a href="">
-                <img src="../admin/modules/quanlysp/uploads/<?php echo $row_pro['hinhanh'] ?>" alt="">
-                <p class="title_product">Tên món : <?php echo $row_pro['tensanpham'] ?></p>
-                <p class="price_product">giá: <?php echo number_format($row_pro['giasp'], 0, ',', '.') . 'vnđ' ?></p>
+    ?> 
+        <li class="product_items">
+        <a href="sanpham.php?quanly=sanpham&id=<?php echo $row['id_sanpham'] ?>" class="product_item">
+                <img src="../admin/modules/quanlysp/uploads/<?php echo $row_pro['hinhanh'] ?>" alt="" ;>
+                <p class="title_product text_edit1"> <?php echo $row_pro['tensanpham'] ?></p>
+                <p class="price_product text_edit1"><?php echo number_format($row_pro['giasp'], 0, ',', '.') . 'vnđ' ?></p>
             </a>
         </li>
     <?php
     }
     ?>
 </ul>
+</div>
