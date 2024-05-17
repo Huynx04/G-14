e4485ffe7a12e0c86023f39af43016f532de9a8f
listimdn10 <121751254+listimdn10@users.noreply.github.com>
2024-05-17 17:58:23 +0700

    Update spheader.php

--- cookable/pages/spheader.php
+++ cookable/pages/spheader.php
@@ -1,1 +1,64 @@
-<div class="header"></div>
+   <nav class="header navbar navbar-expand-sm bg-dark navbar-dark navbar-inverse">
+        <div class="container-fluid">
+            <div class="logo0 col-1  ">
+                <a class="navbar-brand fa-solid fa-truck-fast" href="../main/user.php"></a>
+            </div>
+            <div class="logo1 col-7 pa-y-2 " > 
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-left" id="collapsibleNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link " href="../index.php">Trang chủ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled active " href="img4.php">Sản phẩm</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link " href="../main/blog.php">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../main/lienhe.php">Liên hệ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../main/calo.php">Tính Calo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="../main/congthuc.php">Công thức nấu ăn</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="logo2 col-1">
+                <a class="navbar-brand1 fa-solid fa-user " href="../main/user.php"></a>
+                <a class="navbar-brand2 fa-solid fa-cart-shopping" href="">
+                    <span>0</span>
+                </a>
+            </div>
+        </div>
+    </nav>
+    
+<!--     
+    <div class="banner container-fluid">
+        <div class="col-xxl-7 box-left">
+            <h2>
+                <span>DIỄN ĐÀN - BÀI VIẾT</span>
+            </h2>
+            <p>Nơi sẻ chia công thức và kinh nghiệm tâm đắc của bạn</p>
+            <p>Học hỏi các bí quyết tuyệt vời của người khác</p>
+            <a href="../main/blog_post.php">
+            <button> Đăng bài </button>
+            </a>
+        </div>
+        <div class="col-xl-5 box-right">
+            <img src="https://github.com/hqteamobi/web_shop_ban_hang/blob/main/assets/img_1.png?raw=true" alt="">
+            <img src="https://github.com/hqteamobi/web_shop_ban_hang/blob/main/assets/img_2.png?raw=true" alt="">
+            <img src="https://github.com/hqteamobi/web_shop_ban_hang/blob/main/assets/img_3.png?raw=true" alt="">
+        </div>
+        <div class="to-bottom">
+            <a href="../main/banner.php">
+                <img src="https://github.com/hqteamobi/web_shop_ban_hang/blob/main/assets/to_bottom.png?raw=true" alt="">
+            </a>
+        </div>
+    </div>  -->
