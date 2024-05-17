589f63994ee2311fff58100d2d1b1f9afbef7770
listimdn10 <121751254+listimdn10@users.noreply.github.com>
2024-05-17 22:56:41 +0700

    Update danhmuc.php

--- cookable/pages/main/danhmuc.php
+++ cookable/pages/main/danhmuc.php
@@ -1,4 +1,4 @@
-<body class="showCart">
+<body class="">
 <?php
 $sql_pro = "SELECT * FROM tbl_sanpham WHERE tbl_sanpham.id_danhmuc='$_GET[id]' ORDER BY id_sanpham DESC";
 $query_pro = mysqli_query($mysqli, $sql_pro);
@@ -7,7 +7,7 @@ $query_cate = mysqli_query($mysqli, $sql_cate);
 $row_title = mysqli_fetch_array($query_cate);
 ?>
 
-<h3 style="margin-top: 6%; margin-bottom: 9%; text-align: center; font-size:45px">Danh mục món ăn: <?php echo $row_title['tendanhmuc'] ?> </h3>
+<h1 style="margin-top: 8%; margin-bottom: 5%; text-align: center; font-size:45px">Danh mục món ăn: <?php echo $row_title['tendanhmuc'] ?> </h1>
 <ul class="product_list">
     <?php
     while ($row_pro = mysqli_fetch_array($query_pro)) {
