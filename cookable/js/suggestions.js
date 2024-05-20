3d6605493849e7bdbc3ecb343b38752e7b7ddbfa
listimdn10 <121751254+listimdn10@users.noreply.github.com>
2024-05-20 18:27:14 +0700

    Create suggestions.js

---
+++ cookable/js/suggestions.js
@@ -0,0 +1,17 @@
+let suggestions = [
+    "Mì trộn trứng",
+    "Mì trộn trứng xúc xích",
+    "Mì trộn sốt bơ tỏi",
+    "Bánh mì" ,
+    "Bánh bao khoai môn",
+    "Bánh giò",
+    "Bánh bao trứng múi xá xiếu",
+    "Đùi gà chiên",
+    "Đùi gà sốt mắm tỏi", 
+    "Đùi gà góc tư xối mỡ",
+    "Nước ép nho",
+    "Nước ép táo",
+    "Hamburger bò",
+    "Hamburger gà",
+];
+
