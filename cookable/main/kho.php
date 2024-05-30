9df6218edc37cca9c2b0e37b27f7190ca91eb404
Nolann1110 <163117270+Nolann1110@users.noreply.github.com>
2024-05-30 22:30:23 +0700

    Update kho.php

--- cookable/main/kho.php
+++ cookable/main/kho.php
@@ -9,10 +9,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="../css/kho.css">
-    <link rel="stylesheet" href="chatbot.css">
+    <link rel="stylesheet" href="../css/chatbot.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0" />
-    <script src="chatbot.js" defer></script>
+    <link rel="shortcut icon" type="image/jpg" href="../imgs/icon.jpg"/>
+    <script src="../js/chatbot.js" defer></script>
    
 
 </head>
@@ -36,7 +37,7 @@
                             <a class="nav-link " href="../index.php">Trang chủ</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="img4.php">Sản phẩm</a>
+                            <a class="nav-link" href="../mainphu/sanpham.php">Sản phẩm</a>
                         </li>
                         <li class="nav-item ">
                             <a class="nav-link " href="blog.php">Blog</a>
@@ -55,7 +56,7 @@
             </div>
             <div class="logo2 col-1">
                 <a class="navbar-brand fa-solid fa-user " href="user.php"></a>
-                <a class="navbar-brand fa-solid fa-cart-shopping" href="cart.php"></a>
+                <a class="navbar-brand fa-solid fa-cart-shopping" href="../mainphu/pages/main/giohang.php"></a>
             </div>
         </div>
     </nav>
@@ -64,11 +65,11 @@
     <div class="banner container-fluid">
         <div class="col-xxl-7 box-left">
             <h2>
-                <span>DIỄN ĐÀN - BÀI VIẾT</span>
+                <span>CÔNG THỨC - KHO</span>
             </h2>
             <p>Nơi sẻ chia công thức và kinh nghiệm tâm đắc của bạn</p>
             <p>Học hỏi các bí quyết tuyệt vời của người khác</p>
-            <a href="blog_post.php">
+            <a href="blog.php">
             <button> Đăng bài </button>
             </a>
         </div>
@@ -202,7 +203,7 @@
             <h3>NỘI DUNG</h3>
             <div class="menu list-group-flush">
                 <a href="../index.php" class="list-group-item list-group-item-action">Trang chủ</a>
-                <a href="img4.php" class="list-group-item list-group-item-action">Sản phẩm</a>
+                <a href="../mainphu/sanpham.php" class="list-group-item list-group-item-action">Sản phẩm</a>
                 <a href="blog.php" class="list-group-item list-group-item-action">Blog</a>
                 <a href="lienhe.php" class="list-group-item list-group-item-action">Liên hệ</a>
                 <a href="calo.php" class="list-group-item list-group-item-action">Tính calo</a>
@@ -232,7 +233,7 @@
       </button>
       <div class="chatbot">
         <header>
-          <h2 style="color:aliceblue; margin-top: 4px;">ChatBot</h2>
+          <h2 style="color:aliceblue; margin-top: 4px; margin-bottom: 8px">ChatBot</h2>
           <span class="close-btn material-symbols-outlined">close</span>
         </header>
         <ul class="chatbox">
