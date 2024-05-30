e827e0b0da19a91a21b7d78d02b1465a35533731
Nolann1110 <163117270+Nolann1110@users.noreply.github.com>
2024-05-30 22:21:01 +0700

    Update donuong.php

--- cookable/main/donuong.php
+++ cookable/main/donuong.php
@@ -9,10 +9,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="../css/donuong.css">
-    <link rel="stylesheet" href="chatbot.css">
+    <link rel="stylesheet" href="../css/chatbot.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0" />
-    <script src="chatbot.js" defer></script>
+    <link rel="shortcut icon" type="image/jpg" href="../imgs/icon.jpg"/>
+    <script src="../js/chatbot.js" defer></script>
 </head>
 
 <body>
@@ -33,7 +34,7 @@
                             <a class="nav-link " href="../index.php">Trang chủ</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="img4.php">Sản phẩm</a>
+                            <a class="nav-link" href="../mainphu/sanpham.php">Sản phẩm</a>
                         </li>
                         <li class="nav-item ">
                             <a class="nav-link " href="blog.php">Blog</a>
@@ -52,7 +53,7 @@
             </div>
             <div class="logo2 col-1">
                 <a class="navbar-brand fa-solid fa-user " href="user.php"></a>
-                <a class="navbar-brand fa-solid fa-cart-shopping" href="cart.php"></a>
+                <a class="navbar-brand fa-solid fa-cart-shopping" href="../mainphu/pages/main/giohang.php"></a>
             </div>
         </div>
     </nav>
@@ -61,11 +62,11 @@
     <div class="banner container-fluid">
         <div class="col-xxl-7 box-left">
             <h2>
-                <span>DIỄN ĐÀN - BÀI VIẾT</span>
+                <span>CÔNG THỨC - NƯỚNG</span>
             </h2>
             <p>Nơi sẻ chia công thức và kinh nghiệm tâm đắc của bạn</p>
             <p>Học hỏi các bí quyết tuyệt vời của người khác</p>
-            <a href="blog_post.php">
+            <a href="blog.php">
             <button> Đăng bài </button>
             </a>
         </div>
@@ -95,7 +96,7 @@
                 <div class="card-body">
                     <h5 class="card-title">Sườn nướng chả</h5>
                     <p class="card-text">Sườn được ướp gia vị tự nhiên, sau đó nướng trên lửa than cho đến khi chín vàng. Khi ăn, bạn sẽ cảm nhận được vị ngọt của sườn cùng với độ béo của trứng, tạo nên một trải nghiệm ẩm thực đầy hấp dẫn.</p>
-                    <a href="main/scr_nuong1.php" class="btn btn-primary">View Recipe</a>
+                    <a href="scr_nuong1.php" class="btn btn-primary">View Recipe</a>
                 </div>
             </div>
         </div>
@@ -105,7 +106,7 @@
                 <div class="card-body">
                     <h5 class="card-title">tôm nướng bơ tỏi</h5>
                     <p class="card-text"> Hương vị của tôm ngọt tự nhiên kết hợp hoàn hảo với hương thơm của bơ và tỏi, tạo nên một trải nghiệm ẩm thực độc đáo và hấp dẫn..</p>
-                    <a href="main/scr_nuong2.php" class="btn btn-primary">View Recipe</a>
+                    <a href="scr_nuong2.php" class="btn btn-primary">View Recipe</a>
                 </div>
             </div>
         </div>
@@ -116,7 +117,7 @@
                     <h5 class="card-title">Vịt quay bắc kinh</h5>
                     <p class="card-text"> Vịt được ướp gia vị đặc biệt, sau đó nướng trên lửa than cho đến khi lớp da ngoài giòn tan và vàng óng. Khi ăn, bạn sẽ cảm nhận được hương vị đặc trưng của vịt, kết hợp với độ giòn rụm của lớp da
                     </p>
-                    <a href="main/scr_nuong3.php" class="btn btn-primary">View Recipe</a>
+                    <a href="scr_nuong3.php" class="btn btn-primary">View Recipe</a>
                 </div>
             </div>
         </div>
@@ -143,7 +144,7 @@
    
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <a href="main/blog_post.php">
+            <a href="blog_post.php">
             <img src="https://bcp.cdnchinhphu.vn/334894974524682240/2023/12/6/20200513165141155039rau-bina-la-rau-gi-max-1800x1800-17018491338671987365707.jpg" alt="Rau xanh tốt như thế nào?" class="d-block" style="width:100%">
             <div class="carou-cap">
                 <h3>Rau xanh tốt như thế nào?</h3>
@@ -152,7 +153,7 @@
             </a>
           </div>
           <div class="carousel-item">
-          <a href="main/blog_post.php">
+          <a href="blog_post.php">
             <img src="https://wallpapercave.com/wp/wp6557433.jpg" alt="Sự diệu kì của dâu tây" class="d-block" style="width:100%">
             <div class="carou-cap">
                 <h3>Sự diệu kì của dâu tây</h3>
@@ -161,7 +162,7 @@
             </a>
           </div>
           <div class="carousel-item">
-          <a href="main/blog_post.php">
+          <a href="blog_post.php">
             <img src="https://justcook.butcherbox.com/wp-content/uploads/2020/03/dinner2.jpg" alt="Tự nấu ngon như ở nhà hàng!" class="d-block" style="width:100%">
             <div class="carou-cap">
                 <h3>Tự nấu ngon như ở nhà hàng!<h3>
@@ -197,7 +198,7 @@
             <h3>NỘI DUNG</h3>
             <div class="menu list-group-flush">
                 <a href="../index.php" class="list-group-item list-group-item-action">Trang chủ</a>
-                <a href="img4.php" class="list-group-item list-group-item-action">Sản phẩm</a>
+                <a href="../mainphu/sanpham.php" class="list-group-item list-group-item-action">Sản phẩm</a>
                 <a href="blog.php" class="list-group-item list-group-item-action">Blog</a>
                 <a href="lienhe.php" class="list-group-item list-group-item-action">Liên hệ</a>
                 <a href="calo.php" class="list-group-item list-group-item-action">Tính calo</a>
@@ -227,7 +228,7 @@
       </button>
       <div class="chatbot">
         <header>
-          <h2 style="color:aliceblue; margin-top: 4px;">ChatBot</h2>
+          <h2 style="color:aliceblue; margin-top: 4px; margin-bottom: 8px">ChatBot</h2>
           <span class="close-btn material-symbols-outlined">close</span>
         </header>
         <ul class="chatbox">
