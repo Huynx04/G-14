063a3a11cb2a32945ff02a33769eed16a9060d9f
Nolann1110 <163117270+Nolann1110@users.noreply.github.com>
2024-05-30 22:20:44 +0700

    Update coupon.php

--- cookable/main/coupon.php
+++ cookable/main/coupon.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trang Coupon</title>
     <link rel="stylesheet" href="../css/stylecoupun.css">
+    <link rel="shortcut icon" type="image/jpg" href="../imgs/icon.jpg"/>
 </head>
 <body>
     <header>
