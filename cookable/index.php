bb3b407de44203eb0913d476c8580a5b956aa775
Nolann1110 <163117270+Nolann1110@users.noreply.github.com>
2024-05-30 22:07:50 +0700

    Update index.php

--- cookable/index.php
+++ cookable/index.php
@@ -4,12 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>COOKABLE - BLOG</title>
+    <title>COOKABLE - Trang chủ</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/chatbot.css">
+
     <link rel="stylesheet" href="css/index.css">
     <link  rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0" />
+    <link rel="shortcut icon" type="image/jpg" href="imgs/icon.jpg"/>
 </head>
 <body>
 
@@ -20,31 +25,6 @@
     include("pages/main.php");
     include("pages/footer.php");
 ?>
-
-    <!-- Thêm vào cuối tệp index.html -->
-    <div id="chatbot-circle" onclick="toggleChat()">
-    <p> 
-    <i class="fa-solid fa-robot"></i>
-    </p>
-    </div>
-    
-    <div id="chatbot-interface" style="display: none;">
-    <div id="chatbot-messages"></div>
-    <input id="chatbot-input" type="text" placeholder="Type your message here" />
-    </div>
-    
-    <script>
-    function toggleChat() {
-    var chatInterface = document.getElementById('chatbot-interface');
-    if (chatInterface.style.display === "none") {
-    chatInterface.style.display = "block";
-    } 
-    else {
-    chatInterface.style.display = "none";
-    }
-    }
-    </script>
-
 
 </body>
 </html>
